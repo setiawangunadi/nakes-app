c60e8a078550c94f6bbe61f3d317135b4d0ce6b7
sgudprod <sgudproduction@gmail.com>
2023-05-14 08:18:54 +0700

    change server

--- src/server.js
+++ src/server.js
@@ -20,8 +20,8 @@ const init = async () => {
 
     const server = Hapi.server({
         port: process.env.PORT,
-        // host: process.env.HOST,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        host: process.env.HOST,
+        // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
