bfa47628bf099ae94b3deaded03c57d78a4b055e
sgudprod <sgudproduction@gmail.com>
2023-05-14 08:22:29 +0700

    change auth

--- src/api/users/routes.js
+++ src/api/users/routes.js
@@ -11,33 +11,33 @@ const routes = (handler) => [
         method: 'GET',
         path: '/users',
         handler: handler.getUsersHandler,
-        options: {
-            auth: 'nakesapp_jwt',
-        },
+        // options: {
+        //     auth: 'nakesapp_jwt',
+        // },
     },
     {
         method: 'GET',
         path: '/users/{id}',
         handler: handler.getUserByIdHandler,
-        options: {
-            auth: 'nakesapp_jwt',
-        },
+        // options: {
+        //     auth: 'nakesapp_jwt',
+        // },
     },
     {
         method: 'PUT',
         path: '/users/{id}',
         handler: handler.putUserByIdHandler,
-        options: {
-            auth: 'nakesapp_jwt',
-        },
+        // options: {
+        //     auth: 'nakesapp_jwt',
+        // },
     },
     {
         method: 'DELETE',
         path: '/users/{id}',
         handler: handler.deleteUserByIdHandler,
-        options: {
-            auth: 'nakesapp_jwt',
-        },
+        // options: {
+        //     auth: 'nakesapp_jwt',
+        // },
     },
 ];
 
